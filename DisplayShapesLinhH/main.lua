53122eaf49796d59d298972a390d2639ed1e8f9f
s272936 <46288798+s272936@users.noreply.github.com>
2019-02-12 11:35:56 -0500

    Initial commit

---
+++ DisplayShapesLinhH/main.lua
@@ -0,0 +1,11 @@
+-----------------------------------------------------------------------------------------
+-- Title: Displaying Shapes
+-- Name: Linh Ho
+-- Course: ICS2O
+-- This program shows shapes with different colours labeled 
+-- below identifying what type of shape it is.
+-----------------------------------------------------------------------------------------
+
+-- Hide the status bar
+display.setStatusBar(display.HiddenStatusBar)
+
