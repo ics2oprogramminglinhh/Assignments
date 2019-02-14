7800f263de770d508e903af4c520f84a3abdbdad
s272936 <46288798+s272936@users.noreply.github.com>
2019-02-14 09:25:05 -0500

    DisplayingShapesLinhH

--- DisplayShapesLinhH/main.lua
+++ DisplayShapesLinhH/main.lua
@@ -1,6 +1,7 @@
 -----------------------------------------------------------------------------------------
 -- Title: Displaying Shapes
 -- Name: Linh Ho
+-- Date: February 14th, 2019
 -- Course: ICS2O
 -- This program shows shapes with different colours labeled 
 -- below identifying what type of shape it is.
@@ -8,4 +9,38 @@
 
 -- Hide the status bar
 display.setStatusBar(display.HiddenStatusBar)
+
+-- Sets the background colour
+display.setDefault("background," 202/255, 204/255, 206/255)
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+
+local myTriangle
+
+local triangleText
+
+-----------------------------------------------------------------------------------------
+-- TEXT OBJECTS
+-----------------------------------------------------------------------------------------
+
+-- set text colour of the triangle
+myTriangle:
+
+-- draw the triangle (vertices)
+
+-- set the width of the border
+
+-----------------------------------------------------------------------------------------
+-- EXTRA FUNCTIONS
+-----------------------------------------------------------------------------------------
+
+-- Additional shape
+
+-- Display the area of triangle
+
+-- Inserted an image into 4th shape
+
+-- Gradient fill to 1st shape
 
