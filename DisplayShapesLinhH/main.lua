47350649f005d00311f9a347e9e7f1c39451dcd4
s272936 <linh.ho@ocsbstudent.ca>
2019-02-15 22:01:58 -0500

    Update main.lua

--- DisplayShapesLinhH/main.lua
+++ DisplayShapesLinhH/main.lua
@@ -37,9 +37,14 @@ local myPentagon
 local verticesP = {550, 450, 455, 519, 491, 631, 609, 631, 645, 519}
 local pentagonText
 
-local myOctogon
+local myOctagon
 local verticesO = {588, 458, 512, 458, 458, 512, 458, 588, 512, 642, 588, 642, 642, 588, 642, 512}
-local octogonText 
+local octagonText 
+
+local myTrapezoid
+local verticesZ = {144, 89, 213, 89, 266, 25, 88, 25}
+local trapezoidText 
+
 -----------------------------------------------------------------------------------------
 -- TRIANGLE 
 -----------------------------------------------------------------------------------------
@@ -128,34 +133,68 @@ pentagonText = display.newText("Pentagon", 880, 470, Arial, 40)
 -- Set text colour
 pentagonText:setTextColor(249/255, 102/255, 204/255)
 
-----------------------------------------------------------------------------------------
+-- Add Gradient Fill to Pentagon Shape
+local paint = {
+    type = "gradient",
+    color1 = { 1, 0, 0.4 },
+    color2 = { 1, 0, 0, 0.2 },
+    direction = "down"
+}
 
--- Drawing my Octogon
-local myOctogon = display.newPolygon(500, 600, verticesO)
+local myPentagon = display.newPolygon(880, 350, verticesP)
+myPentagon.fill = paint
 
--- Set the colour of the Octogon
-myOctogon:setStrokeColor(1,1,1)
+-----------------------------------------------------------------------------------------
+
+-- Drawing my Octagon
+local myOctagon = display.newPolygon(500, 600, verticesO)
+
+-- Set the colour of the Octagon
+myOctagon:setStrokeColor(1,1,1)
 
 -- Set the width of the border
-myOctogon.strokeWidth = 6
+myOctagon.strokeWidth = 6
 
 -- Set the colour of the border
-myOctogon:setFillColor(0.4, 0 , 1)
+myOctagon:setFillColor(0.4, 0 , 1)
 
 -- Display the name of the shape below it
-octogonText = display.newText("Octogon", 500, 730, Arial, 40)
+octagonText = display.newText("Octagon", 500, 730, Arial, 40)
 
 -- Set text colour
-octogonText:setTextColor(107/255, 63/255, 160/255)
+octagonText:setTextColor(107/255, 63/255, 160/255)
+
+-- Add Gradient Fill to Octagon Shape
+local paint = {
+    type = "gradient",
+    color1 = { 0.4, 0, 1},
+    color2 = { 0.2, 0, 0, 1},
+    direction = "down"
+}
+
+local myOctagon = display.newPolygon(500, 600, verticesO)
+myOctagon.fill = paint
+
+-------------------------------------------------------------------------------------------
+
+-- Drawing my Trapezoid
+local myTrapezoid = display.newPolygon(200, 600, verticesZ)
+
+-- Set the colour of the Trapezoid
+myTrapezoid:setStrokeColor(1,1,1)
+
+-- Set the width of the border
+myTrapezoid.strokeWidth = 6
+
+-- Set the colour of the border
+myTrapezoid:setFillColor(0.3, 0.3 , 0.1)
+
+-- Display the name of the shape below it
+trapezoidText = display.newText("Trapezoid", 200, 680, Arial, 40)
+
+-- Set text colour
+trapezoidText:setTextColor(120/255, 150/255, 180/255)
 
 
-----------------------------------------------------------------------------------------
-
--- Display the area of triangle
-
--- Inserted an image into triangle
-
-
--- Gradient fill to 1st shape
 
 
